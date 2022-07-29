8bf381e8230b6a7969e7481141d0ec4f4c8344a6
Yaggo Cavalcante <yaggolc@gmail.com>
2022-07-29 00:58:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+        console.log(elemento.localName);
+    } else{
+        console.log('Elemento não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//enquanto
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+    
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){  
+            tecla.classList.add('ativa');
+        }
+
+    tecla.onkeyup = function()  {
+        tecla.classList.remove('ativa');
+        }
+    }
+}
